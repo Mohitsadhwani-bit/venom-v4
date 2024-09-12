81d9820a382584e26934b6051458e6b5ac7160cf
Mohitsadhwani-bit <mohitsadhwani2002@gmail.com>
2024-09-12 12:50:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7133440623:AAELntoiP3mYrpqcOXTdsobjA7W_YTKA3eQ")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Mohitsadhwani")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1271410743")
 
 
 
